c98224038013e00c1a970a5e044b1d7ce4172fc5
xnopre <xnopre@gmail.com>
2016-04-20 10:18:48 +0200

    Step1 : juste les mots

--- live.js
+++ live.js
@@ -1,43 +1,22 @@
 $(function () {
     
-    function displayTweetsCount(list) {
-        var countMap = _.map(list, function(tag){ return tag[1]; });
-        var count = _.reduce(countMap, function(memo, num){ return memo + num; }, 0);
-        $("#compteur").text(count+" tweets #mixit16");
-    }
-
-    function displayTagsCloud(list) {
+    function displayTags(list) {
         
-        displayTweetsCount(list);
+        var content = "";
         
-        var max = _.max(list, function(tag){ return tag[1]; })[1];
+        list.forEach(function(tag) {
+            content += tag[0] + "<br/>";
+        })
 
-        var options = {
-            list: list,
-            gridSize: Math.round(16 * $('#tags').width() / 1024),
-            weightFactor: Math.ceil(70/max),
-            fontFamily: 'Times, serif',
-            /*color: function (word, weight) {
-                return (weight === 12) ? '#f02222' : '#c09292';
-            },*/
-            rotateRatio: 0.5,
-            //color: '#FFF',
-            color: 'random-light',
-            hover: window.drawBox,
-            backgroundColor: '#000'
-        };
-
-        WordCloud('tags', options);
+        $("#tags").html(content);
     };
     
-    function reloadTwits() {
+    function loadTwits() {
         $.getJSON( "twits.txt", function( list ) {
-            displayTagsCloud(list);
+            displayTags(list);
         });
     }
 
-    window.setInterval(reloadTwits, 15000);
-    
-    reloadTwits();
+    loadTwits();
     
 });
